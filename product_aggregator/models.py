b19623717b2f0d1a3eeaa2c9d362d3aee073152b
Václav Hůla <ax@natur.cuni.cz>
2019-12-27 03:05:59 +0100

    Remove spurious comment

--- product_aggregator/models.py
+++ product_aggregator/models.py
@@ -29,7 +29,7 @@ class Product(TrackedRecord):
 
 class Offer(TrackedRecord):
     """product is offered for some price somewhere"""
-    remote_id = models.IntegerField()  # doc does not
+    remote_id = models.IntegerField()
     price = models.IntegerField()
     items_in_stock = models.IntegerField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='offers')
